2372bd8a491869d29512dfa404115cbf3d977397
Adriano Santos <adrianodev2025@outlook.com>
2025-08-10 15:14:59 -0300

    correção da URL da API para o render

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../../services/api";
-import "./Login.css"; // Importa o CSS
+import "./Login.css";
 
 function Login() {
   const [usuario, setUsuario] = useState("");
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+
+// A URL base da tua API hospedada no Render
 const api = axios.create({
-    baseURL: "http://localhost:3001",
+ baseURL: "https://portfolio-backend-0k7x.onrender.com",
 });
 
 export default api;
